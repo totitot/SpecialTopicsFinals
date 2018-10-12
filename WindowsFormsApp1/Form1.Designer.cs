8064178dff8519114bf2c8e3b2f38c703b8d7b21
silladore <e19293001@gmail.com>
2018-10-12 14:33:27 +0800

    added image labels

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.sourcebox1 = new Accord.Controls.PictureBox();
-            this.pictureBox1 = new Accord.Controls.PictureBox();
             this.sourcebox2 = new Accord.Controls.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.resultbox1 = new Accord.Controls.PictureBox();
@@ -47,8 +46,11 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.sourcebox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.sourcebox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.resultbox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.resultbox2)).BeginInit();
@@ -61,28 +63,19 @@
             // 
             this.sourcebox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.sourcebox1.Image = null;
-            this.sourcebox1.Location = new System.Drawing.Point(15, 19);
+            this.sourcebox1.Location = new System.Drawing.Point(6, 40);
             this.sourcebox1.Name = "sourcebox1";
-            this.sourcebox1.Size = new System.Drawing.Size(260, 344);
+            this.sourcebox1.Size = new System.Drawing.Size(260, 347);
             this.sourcebox1.TabIndex = 0;
             this.sourcebox1.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = null;
-            this.pictureBox1.Location = new System.Drawing.Point(-23, -46);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
             // 
             // sourcebox2
             // 
             this.sourcebox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.sourcebox2.Image = null;
-            this.sourcebox2.Location = new System.Drawing.Point(299, 19);
+            this.sourcebox2.Location = new System.Drawing.Point(290, 40);
             this.sourcebox2.Name = "sourcebox2";
-            this.sourcebox2.Size = new System.Drawing.Size(260, 344);
+            this.sourcebox2.Size = new System.Drawing.Size(260, 347);
             this.sourcebox2.TabIndex = 2;
             this.sourcebox2.TabStop = false;
             // 
@@ -94,17 +87,17 @@
             // 
             this.resultbox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.resultbox1.Image = null;
-            this.resultbox1.Location = new System.Drawing.Point(22, 19);
+            this.resultbox1.Location = new System.Drawing.Point(24, 40);
             this.resultbox1.Name = "resultbox1";
-            this.resultbox1.Size = new System.Drawing.Size(294, 344);
+            this.resultbox1.Size = new System.Drawing.Size(294, 347);
             this.resultbox1.TabIndex = 3;
             this.resultbox1.TabStop = false;
             // 
             // sourcebox_btn
             // 
-            this.sourcebox_btn.Location = new System.Drawing.Point(135, 408);
+            this.sourcebox_btn.Location = new System.Drawing.Point(135, 424);
             this.sourcebox_btn.Name = "sourcebox_btn";
-            this.sourcebox_btn.Size = new System.Drawing.Size(59, 28);
+            this.sourcebox_btn.Size = new System.Drawing.Size(59, 53);
             this.sourcebox_btn.TabIndex = 4;
             this.sourcebox_btn.Text = "...";
             this.sourcebox_btn.UseVisualStyleBackColor = true;
@@ -112,9 +105,9 @@
             // 
             // source2box_btn
             // 
-            this.source2box_btn.Location = new System.Drawing.Point(441, 408);
+            this.source2box_btn.Location = new System.Drawing.Point(441, 424);
             this.source2box_btn.Name = "source2box_btn";
-            this.source2box_btn.Size = new System.Drawing.Size(59, 28);
+            this.source2box_btn.Size = new System.Drawing.Size(59, 53);
             this.source2box_btn.TabIndex = 5;
             this.source2box_btn.Text = "...";
             this.source2box_btn.UseVisualStyleBackColor = true;
@@ -122,9 +115,9 @@
             // 
             // process_btn
             // 
-            this.process_btn.Location = new System.Drawing.Point(153, 492);
+            this.process_btn.Location = new System.Drawing.Point(153, 508);
             this.process_btn.Name = "process_btn";
-            this.process_btn.Size = new System.Drawing.Size(59, 28);
+            this.process_btn.Size = new System.Drawing.Size(59, 53);
             this.process_btn.TabIndex = 6;
             this.process_btn.Text = "Process";
             this.process_btn.UseVisualStyleBackColor = true;
@@ -134,7 +127,7 @@
             // 
             this.freak_label.AutoSize = true;
             this.freak_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.freak_label.Location = new System.Drawing.Point(310, 492);
+            this.freak_label.Location = new System.Drawing.Point(276, 517);
             this.freak_label.Name = "freak_label";
             this.freak_label.Size = new System.Drawing.Size(94, 18);
             this.freak_label.TabIndex = 7;
@@ -152,7 +145,7 @@
             // 
             this.simil1.AutoSize = true;
             this.simil1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.simil1.Location = new System.Drawing.Point(429, 492);
+            this.simil1.Location = new System.Drawing.Point(395, 517);
             this.simil1.Name = "simil1";
             this.simil1.Size = new System.Drawing.Size(21, 18);
             this.simil1.TabIndex = 9;
@@ -162,9 +155,9 @@
             // 
             this.resultbox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.resultbox2.Image = null;
-            this.resultbox2.Location = new System.Drawing.Point(339, 19);
+            this.resultbox2.Location = new System.Drawing.Point(341, 40);
             this.resultbox2.Name = "resultbox2";
-            this.resultbox2.Size = new System.Drawing.Size(294, 344);
+            this.resultbox2.Size = new System.Drawing.Size(294, 347);
             this.resultbox2.TabIndex = 10;
             this.resultbox2.TabStop = false;
             // 
@@ -172,7 +165,7 @@
             // 
             this.phash_label.AutoSize = true;
             this.phash_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.phash_label.Location = new System.Drawing.Point(310, 510);
+            this.phash_label.Location = new System.Drawing.Point(276, 535);
             this.phash_label.Name = "phash_label";
             this.phash_label.Size = new System.Drawing.Size(96, 18);
             this.phash_label.TabIndex = 11;
@@ -182,7 +175,7 @@
             // 
             this.simil2.AutoSize = true;
             this.simil2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.simil2.Location = new System.Drawing.Point(429, 510);
+            this.simil2.Location = new System.Drawing.Point(395, 535);
             this.simil2.Name = "simil2";
             this.simil2.Size = new System.Drawing.Size(21, 18);
             this.simil2.TabIndex = 9;
@@ -212,40 +205,80 @@
             // 
             this.groupBox1.Controls.Add(this.angle_label);
             this.groupBox1.Controls.Add(this.angle_text);
-            this.groupBox1.Location = new System.Drawing.Point(289, 458);
+            this.groupBox1.Location = new System.Drawing.Point(255, 483);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(434, 92);
+            this.groupBox1.Size = new System.Drawing.Size(434, 87);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Results";
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.sourcebox2);
             this.groupBox2.Controls.Add(this.sourcebox1);
             this.groupBox2.Location = new System.Drawing.Point(12, 25);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(576, 374);
+            this.groupBox2.Size = new System.Drawing.Size(576, 393);
             this.groupBox2.TabIndex = 13;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Source Images";
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
             this.groupBox3.Controls.Add(this.resultbox1);
             this.groupBox3.Controls.Add(this.resultbox2);
             this.groupBox3.Location = new System.Drawing.Point(608, 25);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(652, 374);
+            this.groupBox3.Size = new System.Drawing.Size(652, 393);
             this.groupBox3.TabIndex = 14;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Output Images";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Template";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(287, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Image";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "FREAK";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(338, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "PHASH";
             // 
             // SpecialTopicsFinals
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1305, 571);
+            this.ClientSize = new System.Drawing.Size(1305, 606);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.phash_label);
             this.Controls.Add(this.simil2);
@@ -255,21 +288,21 @@
             this.Controls.Add(this.process_btn);
             this.Controls.Add(this.source2box_btn);
             this.Controls.Add(this.sourcebox_btn);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox3);
             this.Name = "SpecialTopicsFinals";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.sourcebox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.sourcebox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.resultbox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.resultbox2)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -278,7 +311,6 @@
         #endregion
 
         private Accord.Controls.PictureBox sourcebox1;
-        private Accord.Controls.PictureBox pictureBox1;
         private Accord.Controls.PictureBox sourcebox2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private Accord.Controls.PictureBox resultbox1;
@@ -296,6 +328,10 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
     }
 }
 
